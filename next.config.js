de03205d68ef20c7f17ef07d22a59fa7a593ebc7
Nenad Krneta <shonely@gmail.com>
2021-10-24 09:58:06 +0200

    add sentry

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const { i18n } = require("./next-i18next.config");
+const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = {
-  i18n,
-  target: "experimental-serverless-trace",
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
+
+module.exports = withSentryConfig({
+  i18n,
+  // target: "experimental-serverless-trace",
+};, sentryWebpackPluginOptions);
