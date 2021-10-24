a0d3254726e5d62815df850483fd13c6b5012419
Nenad Krneta <shonely@gmail.com>
2021-10-24 16:03:00 +0200

    remove sentry

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 const { i18n } = require("./next-i18next.config");
-const { withSentryConfig } = require("@sentry/nextjs");
+// const { withSentryConfig } = require("@sentry/nextjs");
+// const withPlugins = require("next-compose-plugins");
+// // import withTranspileModules from 'next-transpile-modules'
+// const optimizedImages = require("next-optimized-images");
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
+// const sentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, org, project, authToken, configFile, stripPrefix,
+//   //   urlPrefix, include, ignore
 
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+//   silent: true, // Suppresses all logs
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// };
 
 // module.exports = withSentryConfig(
 //   {
@@ -20,7 +23,21 @@ const sentryWebpackPluginOptions = {
 //   },
 //   sentryWebpackPluginOptions
 // );
+const plugins = [
+  // withBundleAnalyzer,
+  [
+    optimizedImages,
+    {
+      handleImages: ["jpeg", "png", "svg", "webp", "gif"],
+      optimizeImages: true,
+      optimizeImagesInDev: false,
+    },
+  ],
+  // withTM,
+];
 
-module.exports = {
-  i18n,
-};
+// module.exports = withPlugins(plugins, {
+//   i18n,
+// });
+
+module.exports = { i18n };
